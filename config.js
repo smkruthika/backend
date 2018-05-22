5614cc4c23630604ab1c24225554a263b2836c25
Kruthika S M (krsm) <krsm@cisco.com>
2018-05-22 15:27:19 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '1811743505555502',
+  clientSecret: 'a514ac87d3b67df52c6d00a725831334',
+  callbackURL: 'https://localhost/auth/facebook/callback',//https://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '347075385689-diue77ta969ei1co6sndnuakd46lge8g.apps.googleusercontent.com',
+  clientSecret: '4FE-3vT6fXtFSUxtQ1nicVBw',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
